65b94f1767ffe45b303212a87a2be939129f9ff5
Juan Antonio <Martinezja86@gmail.com>
2024-09-21 16:44:39 +0200

    creado SPA app testeando views

---
+++ assests/js/mainScript.js
@@ -0,0 +1,214 @@
+function handleRouteChange() {
+	const path = window.location.pathname;
+	let view;
+	switch (path) {
+		case '/Profile':
+		view = aboutView();
+		break;
+	default:
+		view = '<h1>404</h1>';
+	}
+	document.getElementById('app').innerHTML = view;
+}
+
+function homeView() {
+	return '<h1>Home Page</h1>';
+}
+
+function aboutView() {
+	return `<div class="app" style="background-color: white;">
+			<div class="bar_nav_flex">
+				<div class="bar_nav">
+					<button type="button" class="btn btn-primary">High Scores</button>
+					<button type="button" class="btn btn-primary">My Scores</button>
+					<button type="button" class="btn btn-primary">Game Statistics</button>
+					<button type="button" class="btn btn-primary">Friends</button>
+					<button type="button" class="btn btn-primary">Chat</button>
+				</div>
+			</div>
+			<div class="content_nav">
+				<div class="table-container">
+					<table class="table table-dark table-striped table-hover">
+						<thead>
+							<tr>
+							<th scope="col">Position</th>
+							<th scope="col">User Name</th>
+							<th scope="col">Score</th>
+							<th scope="col">Date</th>
+							</tr>
+						</thead>
+						<tbody>
+							<tr>
+							<th scope="row">1</th>
+								<td>juan-anm</td>
+								<td>800px</td>
+								<td>16/09/2024</td>
+							</tr>
+							<tr>
+							<th scope="row">2</th>
+							<td>popeye</td>
+							<td>750px</td>
+							<td>16/09/2024</td>
+							</tr>
+							<tr>
+								<th scope="row">3</th>
+								<td>pacote</td>
+								<td>40px</td>
+								<td>16/09/2024</td>
+							</tr>
+							<tr>
+								<th scope="row">3</th>
+								<td>pacote</td>
+								<td>40px</td>
+								<td>16/09/2024</td>
+							</tr>
+							<tr>
+								<th scope="row">3</th>
+								<td>pacote</td>
+								<td>40px</td>
+								<td>16/09/2024</td>
+							</tr>
+							<tr>
+								<th scope="row">3</th>
+								<td>pacote</td>
+								<td>40px</td>
+								<td>16/09/2024</td>
+							</tr>
+							<tr>
+								<th scope="row">3</th>
+								<td>pacote</td>
+								<td>40px</td>
+								<td>16/09/2024</td>
+							</tr>
+							<tr>
+								<th scope="row">3</th>
+								<td>pacote</td>
+								<td>40px</td>
+								<td>16/09/2024</td>
+							</tr>
+							<tr>
+								<th scope="row">3</th>
+								<td>pacote</td>
+								<td>40px</td>
+								<td>16/09/2024</td>
+							</tr>
+							<tr>
+								<th scope="row">3</th>
+								<td>pacote</td>
+								<td>40px</td>
+								<td>16/09/2024</td>
+							</tr>
+							<tr>
+								<th scope="row">3</th>
+								<td>pacote</td>
+								<td>40px</td>
+								<td>16/09/2024</td>
+							</tr>
+							<tr>
+								<th scope="row">1</th>
+									<td>juan-anm</td>
+									<td>800px</td>
+									<td>16/09/2024</td>
+								</tr>
+							<tr>
+								<th scope="row">2</th>
+								<td>popeye</td>
+								<td>750px</td>
+								<td>16/09/2024</td>
+							</tr>
+							<tr>
+								<th scope="row">3</th>
+								<td>pacote</td>
+								<td>40px</td>
+								<td>16/09/2024</td>
+							</tr>
+							<tr>
+									<th scope="row">3</th>
+									<td>pacote</td>
+									<td>40px</td>
+									<td>16/09/2024</td>
+								</tr>
+								<tr>
+									<th scope="row">3</th>
+									<td>pacote</td>
+									<td>40px</td>
+									<td>16/09/2024</td>
+								</tr>
+								<tr>
+									<th scope="row">3</th>
+									<td>pacote</td>
+									<td>40px</td>
+									<td>16/09/2024</td>
+								</tr>
+								<tr>
+									<th scope="row">3</th>
+									<td>pacote</td>
+									<td>40px</td>
+									<td>16/09/2024</td>
+								</tr>
+								<tr>
+									<th scope="row">3</th>
+									<td>pacote</td>
+									<td>40px</td>
+									<td>16/09/2024</td>
+								</tr>
+								<tr>
+									<th scope="row">3</th>
+									<td>pacote</td>
+									<td>40px</td>
+									<td>16/09/2024</td>
+								</tr>
+								<tr>
+									<th scope="row">3</th>
+									<td>pacote</td>
+									<td>40px</td>
+									<td>16/09/2024</td>
+								</tr>
+								<tr>
+									<th scope="row">3</th>
+									<td>pacote</td>
+									<td>40px</td>
+									<td>16/09/2024</td>
+								</tr>
+								<tr>
+									<th scope="row">3</th>
+									<td>pacote</td>
+									<td>40px</td>
+									<td>16/09/2024</td>
+								</tr>
+								<tr>
+									<th scope="row">3</th>
+									<td>pacote</td>
+									<td>40px</td>
+									<td>16/09/2024</td>
+								</tr>
+								<tr>
+									<th scope="row">3</th>
+									<td>pacote</td>
+									<td>40px</td>
+									<td>16/09/2024</td>
+								</tr>
+								<tr>
+									<th scope="row">3</th>
+									<td>LAST</td>
+									<td>486666</td>
+									<td>16/09/2024</td>
+								</tr>
+						</tbody>
+					</table>
+				</div>
+			</div>	
+		</div>`;
+}
+
+window.addEventListener('popstate', handleRouteChange);
+
+document.querySelectorAll('.route').forEach(link => {
+	link.addEventListener('click', function(e) {
+		e.preventDefault();
+		history.pushState(null, '', this.href);
+		handleRouteChange();
+	});
+});
+
+handleRouteChange();
